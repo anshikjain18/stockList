a156430fbaefd27ea69b5ecdb74f63d141ce0a60
Anshik Jain <anshikjain18@gmail.com>
2024-07-28 19:14:22 +0530

    setting base href to serve UI at /stocklist

--- stockListUI/src/app/app.config.ts
+++ stockListUI/src/app/app.config.ts
@@ -6,9 +6,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
 import {authInterceptor} from "./auth/auth.interceptor";
+import {APP_BASE_HREF} from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: APP_BASE_HREF, useValue: '/stocklist/' },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
