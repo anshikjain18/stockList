47098bed81a0a8fdcb5d45a5bf471a22b2f4ecaa
Anshik Jain <anshikjain18@gmail.com>
2024-07-28 12:37:20 +0530

    removing build command from script

--- deployStockList.sh
+++ deployStockList.sh
@@ -15,13 +15,7 @@ source stockList/bin/activate
 # installing python dependencies
 pip install -r requirements.txt
 
-# running backend
-python3 main.py
-
 cd ../stockListUI
 
 # installing npm dependencies
 npm install
-
-# running UI
-ng build
